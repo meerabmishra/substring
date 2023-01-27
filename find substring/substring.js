7d80f4ec38f94a3214075ae6e99c05e8fe00e610
meerabmishra <64734591+meerabmishra@users.noreply.github.com>
2023-01-27 11:36:53 +0530

    Add files via upload

---
+++ find substring/substring.js
@@ -0,0 +1,26 @@
+function longestCommonSubstring(str1,str2){
+    let m= str1.length;
+    let n=str2.length;
+    let result=0;
+    let end=0
+    let len=new Array(m+1);
+    for(let i=0;i<=m;i++){
+        len[i]=new Array(n+1);
+        for(let j=0;j<=n;j++){
+            len[i][j]=0
+        }
+    }
+    for(let i=1;i<=m;i++){
+        for(let j=1;j<=n;j++){
+            if(str1[i-1]==str2[j-1]){
+                len[i][j]=len[i-1][j-1]+1;
+                if(len[i][j]>result){
+                    result=len[i][j];
+                    end=i;
+                }
+            }
+        }
+    }
+    return str1.substring(end-result,end);
+}
+longestCommonSubstring("Meera",'Peeram')
